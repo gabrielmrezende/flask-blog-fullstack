370fe119eb619e6018a92fe9f45f9724e16bcd97
Gabriel Rezende <gabrielrezendee03@gmail.com>
2026-02-28 11:03:54 -0300

    Commit Inicial Do projeto Blog com Flask

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+lista_usuarios = ['Gabriel', 'Ricardo', 'Cynthia', 'Ana Luiza']
+
+app.config['SECRET_KEY'] = '23416eb77c1417fefa75088240681a3c'
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/contato")
+def contato():
+    return render_template('contato.html')
+
+@app.route("/usuarios")
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')    
+
+if __name__ == '__main__':
+    app.run(debug=True)
